cb9a2b63be987179ca550b5cdba9d255fccd4a09
jsdev <sc.sivakorn@gmail.com>
2021-12-17 19:14:33 +0700

    change run to api

--- bot.js
+++ bot.js
@@ -26,118 +26,127 @@ const data = {
   gasLimit : '200000' //at least 21000
 }
 
-let bscMainNetUrl = 'https://bsc-dataseed.binance.org/'
-// let bscTestnetUrl = 'https://data-seed-prebsc-1-s1.binance.org:8545'
-let web3 = new Web3(new Web3.providers.HttpProvider(bscMainNetUrl));
-let provider = new ethers.providers.JsonRpcProvider(bscMainNetUrl);
-let wallet = new ethers.Wallet(privatekey);
-const account = wallet.connect(provider);
+app.get('/sniper/:tokenAddress', async (req, res) => {
+  try {
+    console.log(chalk.yellow(`-------- Start Service ---------`))
+    let bscMainNetUrl = 'https://bsc-dataseed.binance.org/'
+    // let bscTestnetUrl = 'https://data-seed-prebsc-1-s1.binance.org:8545'
+    let web3 = new Web3(new Web3.providers.HttpProvider(bscMainNetUrl));
+    let provider = new ethers.providers.JsonRpcProvider(bscMainNetUrl);
+    let wallet = new ethers.Wallet(privatekey);
+    const account = wallet.connect(provider);
 
-const router = new ethers.Contract(
-  data.router,
-  [
-    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
-  ],
-  account
-);
+    const router = new ethers.Contract(
+      data.router,
+      [
+        'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+        'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+      ],
+      account
+    );
 
-const run = async () => {
-  const tokenIn = data.WBNB;
-  const tokenOut = data.to_PURCHASE;
+    const { tokenAddress } = req.params
+    const tokenIn = data.WBNB;
+    const tokenOut = tokenAddress || '';
+    console.log('contract address = ', tokenAddress)
+    data.to_PURCHASE = tokenAddress
+    console.log(chalk.green(`Connected With Account = `, account))
 
-  const tokenAInput = new web3.eth.Contract(ERC20.abi,data.WBNB);
-  const tokenBPurchase = new web3.eth.Contract(ERC20.abi,data.to_PURCHASE);
-  const address = await account.getAddress()
-  const tokenAResult = await tokenAInput.methods.allowance(address,data.router).call();
-  const tokenBResult = await tokenBPurchase.methods.allowance(address,data.router).call();
+    const tokenAInput = new web3.eth.Contract(ERC20.abi,data.WBNB);
+    const tokenBPurchase = new web3.eth.Contract(ERC20.abi,data.to_PURCHASE);
+    const address = await account.getAddress()
+    const tokenAResult = await tokenAInput.methods.allowance(address,data.router).call();
+    const tokenBResult = await tokenBPurchase.methods.allowance(address,data.router).call();
+    console.log('tokenAResult',tokenAResult)
+    let allowanceTokenA = ethers.utils.formatEther(tokenAResult).toString()
+    let allowanceTokenB = ethers.utils.formatEther(tokenBResult).toString()
 
-  let allowanceTokenA = ethers.utils.formatEther(tokenAResult).toString()
-  let allowanceTokenB = ethers.utils.formatEther(tokenBResult).toString()
+    const user_wallet = web3.eth.accounts.privateKeyToAccount(privatekey);
+    var max_allowance = '10000000000000000000000000';
+    console.log('allowanceTokenA',allowanceTokenA)
+    if(Number(allowanceTokenA) === 0.0){
+      var approveTX ={
+        from: user_wallet.address,
+        to: data.to_PURCHASE,
+        gas: data.gasLimit,
+        gasPrice: 50*ONE_GWEI,
+        data: tokenAInput.methods.approve(data.router, max_allowance).encodeABI()
+      }
 
-  const user_wallet = web3.eth.accounts.privateKeyToAccount(privatekey);
-  var max_allowance = '10000000000000000000000000';
-  console.log('allowanceTokenA',allowanceTokenA)
-  if(Number(allowanceTokenA) === 0.0){
-    var approveTX ={
-      from: user_wallet.address,
-      to: data.to_PURCHASE,
-      gas: data.gasLimit,
-      gasPrice: 50*ONE_GWEI,
-      data: tokenAInput.methods.approve(data.router, max_allowance).encodeABI()
+      var signedTX = await user_wallet.signTransaction(approveTX);
+      var result = await web3.eth.sendSignedTransaction(signedTX.rawTransaction);
+      console.log('result',result)
+      console.log('Approved Token A')
+    }
+    console.log('allowanceTokenB',allowanceTokenB)
+    if(Number(allowanceTokenB) === 0.0){
+      var approveTX ={
+        from: user_wallet.address,
+        to: data.to_PURCHASE,
+        gas: data.gasLimit,
+        gasPrice: 50*ONE_GWEI,
+        data: tokenBPurchase.methods.approve(data.router, max_allowance).encodeABI()
+      }
+
+      var signedTX = await user_wallet.signTransaction(approveTX);
+      var result = await web3.eth.sendSignedTransaction(signedTX.rawTransaction);
+      console.log('result',result)
+      console.log('Approved Token B')
     }
 
-    var signedTX = await user_wallet.signTransaction(approveTX);
-    var result = await web3.eth.sendSignedTransaction(signedTX.rawTransaction);
-    console.log('result',result)
-    console.log('Approved Token A')
-  }
-  console.log('allowanceTokenB',allowanceTokenB)
-  if(Number(allowanceTokenB) === 0.0){
-    var approveTX ={
-      from: user_wallet.address,
-      to: data.to_PURCHASE,
-      gas: data.gasLimit,
-      gasPrice: 50*ONE_GWEI,
-      data: tokenBPurchase.methods.approve(data.router, max_allowance).encodeABI()
+    const amountInChecked = ethers.utils.parseUnits('1', 'ether');
+    const resultAmount = await router.getAmountsOut(amountInChecked, [tokenIn, tokenOut]);
+    const amountChecked = ethers.utils.formatEther(resultAmount[1].toString())
+
+    console.log(chalk.yellow(`CGUN Received = ` + amountChecked))
+    if(Number(amountChecked) > 421){
+      //We buy x amount of the new token for our wbnb
+      const amountIn = ethers.utils.parseUnits(`${data.AMOUNT_OF_WBNB}`, 'ether');
+      const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
+
+      //Our execution price will be a bit different, we need some flexbility
+      const amountOutMin = amounts[1].sub(amounts[1].div(`${data.Slippage}`));
+      console.log(
+        chalk.green.inverse(`Liquidity Addition Detected\n`)
+        +
+        `Buying Token
+       =================
+       tokenIn: ${amountIn.toString()} ${tokenIn} (WBNB)
+       tokenOut: ${amountOutMin.toString()} ${tokenOut}
+     `);
+
+      console.log('amount purchase = ', ethers.utils.formatEther(amountOutMin.toString()))
+
+      console.log('Processing Transaction.....');
+      console.log(chalk.yellow(`amountIn: ${amountIn}`));
+      console.log(chalk.yellow(`amountOutMin: ${amountOutMin}`));
+      console.log(chalk.yellow(`tokenIn: ${tokenIn}`));
+      console.log(chalk.yellow(`tokenOut: ${tokenOut}`));
+      console.log(chalk.yellow(`data.recipient: ${data.recipient}`));
+      console.log(chalk.yellow(`data.gasLimit: ${data.gasLimit}`));
+      console.log(chalk.yellow(`data.gasPrice: ${ethers.utils.parseUnits(`${data.gasPrice}`, 'gwei')}`));
+
+      const tx = await router.swapExactTokensForTokens(
+        amountIn,
+        amountOutMin,
+        [tokenIn, tokenOut],
+        data.recipient,
+        Date.now() + 1000 * 60 * 10, //10 minutes
+        {
+          'gasLimit': data.gasLimit,
+          'gasPrice': ethers.utils.parseUnits(`${data.gasPrice}`, 'gwei')
+        });
+
+      const receipt = await tx.wait();
+      console.log('Transaction receipt');
+      console.log(receipt);
     }
 
-    var signedTX = await user_wallet.signTransaction(approveTX);
-    var result = await web3.eth.sendSignedTransaction(signedTX.rawTransaction);
-    console.log('result',result)
-    console.log('Approved Token B')
+    res.json('success')
+  } catch (err){
+    res.json(err)
   }
-
-  const amountInChecked = ethers.utils.parseUnits('1', 'ether');
-  const resultAmount = await router.getAmountsOut(amountInChecked, [tokenIn, tokenOut]);
-  const amountChecked = ethers.utils.formatEther(resultAmount[0].toString())
-
-  console.log(chalk.yellow(`CGUN Received = ` + amountChecked))
-  if(Number(amountChecked) > 421){
-    //We buy x amount of the new token for our wbnb
-    const amountIn = ethers.utils.parseUnits(`${data.AMOUNT_OF_WBNB}`, 'ether');
-    const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
-
-    //Our execution price will be a bit different, we need some flexbility
-    const amountOutMin = amounts[1].sub(amounts[1].div(`${data.Slippage}`));
-    console.log(
-      chalk.green.inverse(`Liquidity Addition Detected\n`)
-      +
-      `Buying Token
-     =================
-     tokenIn: ${amountIn.toString()} ${tokenIn} (WBNB)
-     tokenOut: ${amountOutMin.toString()} ${tokenOut}
-   `);
-
-    console.log('amount purchase = ', ethers.utils.formatEther(amountOutMin.toString()))
-
-    console.log('Processing Transaction.....');
-    console.log(chalk.yellow(`amountIn: ${amountIn}`));
-    console.log(chalk.yellow(`amountOutMin: ${amountOutMin}`));
-    console.log(chalk.yellow(`tokenIn: ${tokenIn}`));
-    console.log(chalk.yellow(`tokenOut: ${tokenOut}`));
-    console.log(chalk.yellow(`data.recipient: ${data.recipient}`));
-    console.log(chalk.yellow(`data.gasLimit: ${data.gasLimit}`));
-    console.log(chalk.yellow(`data.gasPrice: ${ethers.utils.parseUnits(`${data.gasPrice}`, 'gwei')}`));
-
-    const tx = await router.swapExactTokensForTokens(
-      amountIn,
-      amountOutMin,
-      [tokenIn, tokenOut],
-      data.recipient,
-      Date.now() + 1000 * 60 * 10, //10 minutes
-      {
-        'gasLimit': data.gasLimit,
-        'gasPrice': ethers.utils.parseUnits(`${data.gasPrice}`, 'gwei')
-      });
-
-    const receipt = await tx.wait();
-    console.log('Transaction receipt');
-    console.log(receipt);
-  }
-}
-
-run();
+})
 
 const PORT = 5000;
 
